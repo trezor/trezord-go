11b57871795c34b68f46e68fe5a36ca776e0695e
Pavol Rusnak <stick@gk2.sk>
2018-02-05 19:17:00 +0100

    linux: fix missing plugdev group

--- release/linux/fpm.before-install.sh
+++ release/linux/fpm.before-install.sh
@@ -1,5 +1,6 @@
 getent group trezord >/dev/null || groupadd -r trezord
-getent passwd trezord >/dev/null || useradd -r -g trezord -d /var -s /sbin/nologin -c "TREZOR Bridge" trezord
+getent group plugdev >/dev/null || groupadd -r plugdev
+getent passwd trezord >/dev/null || useradd -r -g trezord -G plugdev -d /var -s /sbin/nologin -c "TREZOR Bridge" trezord
 touch /var/log/trezord.log
 chown trezord:trezord /var/log/trezord.log
 chmod 660 /var/log/trezord.log
