ffb5ffa6613a3b0fffd8959bde75395ab9d3b110
Saleem Rashid <dev@saleemrashid.com>
2018-02-17 19:18:48 +0000

    usb: Add emulator/UDP
    UDP layer is only turned on if there is at least 1 port enabled

--- trezord.go
+++ trezord.go
@@ -5,15 +5,41 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/trezor/trezord-go/server"
 	"github.com/trezor/trezord-go/usb"
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
+type udpPorts []int
+
+func (i *udpPorts) String() string {
+	res := ""
+	for i, p := range *i {
+		if i > 0 {
+			res = res + ","
+		}
+		res = res + strconv.Itoa(p)
+	}
+	return res
+}
+
+func (i *udpPorts) Set(value string) error {
+	p, err := strconv.Atoi(value)
+	if err != nil {
+		return nil
+	}
+	*i = append(*i, p)
+	return nil
+}
+
 func main() {
 	var logfile string
+	var ports udpPorts
+
 	flag.StringVar(&logfile, "l", "", "Log into a file, rotating after 5MB")
+	flag.Var(&ports, "e", "Use UDP port for emulator. Can be repeated for more ports. Example: trezord-go -e 21324 -e 21326")
 	flag.Parse()
 
 	var logger io.WriteCloser
@@ -36,7 +62,18 @@ func main() {
 	if err != nil {
 		log.Fatalf("hidapi: %s", err)
 	}
-	b := usb.Init(w, h)
+
+	var b *usb.USB
+
+	if len(ports) > 0 {
+		e, err := usb.InitUDP(ports)
+		if err != nil {
+			log.Fatalf("emulator: %s", err)
+		}
+		b = usb.Init(w, h, e)
+	} else {
+		b = usb.Init(w, h)
+	}
 
 	s, err := server.New(b, logger)
 	if err != nil {
