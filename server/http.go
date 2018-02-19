f7b3ecd70e2f5bc6e24b1d7a53c23fcc71ff8723
Karel Bilek <kb@karelbilek.com>
2018-02-19 14:18:50 +0100

    version: 2.0.6

--- server/http.go
+++ server/http.go
@@ -132,7 +132,7 @@ func (s *server) Info(w http.ResponseWriter, r *http.Request) {
 		Version string `json:"version"`
 	}
 	json.NewEncoder(w).Encode(info{
-		Version: "2.0.5",
+		Version: "2.0.6",
 	})
 }
 
