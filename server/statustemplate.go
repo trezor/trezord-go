0fe7f2d722408db7b369cf53ec4cf143530ed842
Karel Bilek <kb@karelbilek.com>
2018-03-22 14:09:39 +0100

    New status template

--- server/statustemplate.go
+++ server/statustemplate.go
@@ -25,49 +25,148 @@ type statusTemplateData struct {
 }
 
 const templateString = `
-<html>
-<head><title>TREZOR Bridge status</title></head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>TREZOR Bridge status</title>
+  <style>
+    body {
+      font-family: 'Open sans';
+    }
+
+    h1 {
+      font-size: 36px;
+    }
+
+    p {
+      color: #858585;
+    }
+
+    #container {
+      width: 100%;
+    }
+
+    .item {
+      border: 1px solid lightgray;
+      border-radius: 4px;
+      min-width: 320px;
+      max-width: 500px;
+      min-height: 100px;
+      margin: 20px auto;
+      position: relative;
+    }
+
+    .item h3 {
+      left: 20px;
+      position: absolute;
+    }
+
+    .item p {
+      top: 50px;
+      left: -5px;
+      position: relative;
+      font-size: 12px;
+    }
+
+    .item .session {
+      top: 20px;
+      right: 20px;
+      position: absolute;
+    }
+
+    .item-content {
+      width: 100%;
+    }
+
+    .inner-container {
+      max-width: 1024px;
+      margin: 0 auto;
+      text-align: center;
+      border-radius: 4px;
+    }
+
+    .badge {
+      display: inline-block;
+      padding: 6px 10px 6px 10px;
+      border: 1px solid #01B757;
+      border-radius: 4px;
+      color: #01B757;
+    }
+
+    .heading {
+      margin-bottom: 40px;
+    }
+
+    .space-top {
+      margin-top: 34px;
+    }
+
+    .btn-primary {
+      display: inline-block;
+      padding: 10px 40px 10px 40px;
+      background-color: #01B757;
+      color: white;
+      border-radius: 4px;
+    }
+
+    .btn-primary:hover {
+      background-color: #00A24C;
+    }
+
+    textarea{
+      max-width: 500px;
+    }
+  </style>
+</head>
+
 <body>
-<h3>TREZOR Bridge seems to be working</h3>
-Version: {{.Version}}
-<br><br>
-Device count: {{.DeviceCount}}
-<br><br>
-Devices:
-<br>
-<br>
-{{range .Devices}}
-  
-  <b>
-  {{if eq .Type 0}}
-    TREZOR One
-  {{end}}
+  <div id="container">
+    <div class="inner-container">
+      <div class="heading">
+        <h1>TREZOR Bridge status</h1>
+        <span class="badge">Version: {{.Version}}</span>
+      </div>
 
-  {{if eq .Type 1}}
-    TREZOR Model T
-  {{end}}
-  {{if eq .Type 2}}
-    TREZOR Model T (bootloader)
-  {{end}}
-  </b>
+      <p>Connected devices: {{.DeviceCount}}</p>
 
-  <br>
-  {{.Path}}
-  <br>
+      {{range .Devices}}
+      <div class="item">
+        <h3>{{if eq .Type 0}}
+          TREZOR One
+        {{end}}
 
-  {{if .Used}}
-    Session: {{.Session}}
-  {{end}}
-  {{if not .Used}}
-    Session: no session
-  {{end}}
-  <br><br>
+        {{if eq .Type 1}}
+          TREZOR Model T
+        {{end}}
 
-{{end}}
-<textarea rows="25" cols="80">
+        {{if eq .Type 2}}
+          TREZOR Model T (bootloader)
+        {{end}}
+      </h3>
+        <span class="session">
+        {{if .Used}} Session: {{.Session}} {{end}} {{if not .Used}} Session: no session {{end}}
+        </span>
+        <p>{{.Path}}</p>
+       </div>
+      {{end}}
+
+       <div class="space-top">
+       <p>Console Log</p>
+       <textarea rows="25" cols="80">
 {{.Log}}
-</textarea>
-(You need to reload the page after connecting/disconnecting)
+       </textarea>
+     </div>
+
+      <div class="space-top">
+        <p>You may need to reload the page after connecting / disconnecting</p>
+        <a href="#">
+          <div class="btn-primary">Refresh page</div>
+        </a>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
 `
