dd0f9df0398b231b9569c98c1988c45767132c5d
Thomas Kerin <thomas.kerin@bitmaintech.com>
2018-04-07 14:46:17 +0200

    allow booting in nousb mode

--- trezord.go
+++ trezord.go
@@ -38,9 +38,11 @@ func (i *udpPorts) Set(value string) error {
 func main() {
 	var logfile string
 	var ports udpPorts
+	var withusb bool
 
 	flag.StringVar(&logfile, "l", "", "Log into a file, rotating after 5MB")
 	flag.Var(&ports, "e", "Use UDP port for emulator. Can be repeated for more ports. Example: trezord-go -e 21324 -e 21326")
+	flag.BoolVar(&withusb, "u", true, "Use USB devices. Can be disabled for testing environments. Example: trezord-go -e 21324 -u=false")
 	flag.Parse()
 
 	var logger io.Writer
@@ -60,29 +62,34 @@ func main() {
 	log.SetOutput(logger)
 	log.Println("trezord is starting.")
 
-	w, err := usb.InitWebUSB()
-	if err != nil {
-		log.Fatalf("webusb: %s", err)
-	}
-	defer w.Close()
+	var bus []usb.Bus
+	if withusb {
+		w, err := usb.InitWebUSB()
+		if err != nil {
+			log.Fatalf("webusb: %s", err)
+		}
+		defer w.Close()
 
-	h, err := usb.InitHIDAPI()
-	if err != nil {
-		log.Fatalf("hidapi: %s", err)
+		h, err := usb.InitHIDAPI()
+		if err != nil {
+			log.Fatalf("hidapi: %s", err)
+		}
+		bus = append(bus, w, h)
 	}
-
-	var b *usb.USB
 
 	if len(ports) > 0 {
 		e, errUDP := usb.InitUDP(ports)
 		if errUDP != nil {
 			log.Fatalf("emulator: %s", errUDP)
 		}
-		b = usb.Init(w, h, e)
-	} else {
-		b = usb.Init(w, h)
+		bus = append(bus, e)
 	}
 
+	if len(bus) == 0 {
+		log.Fatalf("No transports enabled")
+	}
+
+	b := usb.Init(bus...)
 	s, err := server.New(b, logger, m)
 	if err != nil {
 		log.Fatalf("https: %s", err)
