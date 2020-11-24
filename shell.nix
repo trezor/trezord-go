4a8ff03a2ab985121b1e56b7823d8fb148119b16
Pavol Rusnak <pavol@rusnak.io>
2020-11-24 14:59:42 +0100

    nix: add Makefile which can build linux-{arm64,x64} and win-x64 targets

--- shell.nix
+++ shell.nix
@@ -1,6 +1,16 @@
 with import <nixpkgs> {};
 
 stdenv.mkDerivation {
-  name = "trezord-go-dev";
-  buildInputs = [ go ];
+  name = "trezord-go-env";
+  buildInputs = [
+    fpm
+    gcc
+    gnupg
+    go
+    nsis
+    osslsigncode
+    pkgsCross.aarch64-multiplatform.buildPackages.gcc
+    pkgsCross.mingwW64.buildPackages.gcc
+    rpm
+  ];
 }
