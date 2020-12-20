4e9dbc37477183bd65a21a2b2394e658a072b9a2
Pavol Rusnak <pavol@rusnak.io>
2020-12-20 20:59:47 +0100

    libusb: reintroduce accidentally removed PRINTF_FORMAT definition

--- usb/lowlevel/libusb/c/config.h
+++ usb/lowlevel/libusb/c/config.h
@@ -1,3 +1,15 @@
 #ifndef CONFIG_H
+
 #define CONFIG_H
+
+#ifndef _MSC_VER
+
+#define PRINTF_FORMAT(a, b) __attribute__ ((__format__ (__printf__, a, b)))
+
+#else
+
+#define PRINTF_FORMAT(a, b)
+
+#endif
+
 #endif
