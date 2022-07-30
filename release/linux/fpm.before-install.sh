ec928e6456bfa60c9a298ae78613f723db256900
Alois Klink <alois@aloisklink.com>
2022-07-31 00:09:40 +0100

    fix(linux): remove /var/log/trezord.log creation
    As far as I'm aware, Linux never logged to /var/log/trezord.log
    (I did a quick search through `git blame` and couldn't find anything)
    
    On my PC, `ls -lhrt /var/log/trezord.log` that shows that it has
    0 bytes, and has been unmodified since 2021.
    
    Instead, Linux just relies on systemd's logger.
    On most new devices, you can view them via `journalctl -u trezord`.

--- release/linux/fpm.before-install.sh
+++ release/linux/fpm.before-install.sh
@@ -2,6 +2,3 @@ getent group trezord >/dev/null || groupadd -r trezord
 getent group plugdev >/dev/null || groupadd -r plugdev
 getent passwd trezord >/dev/null || useradd -r -g trezord -d /var -s /bin/false -c "Trezor Bridge" trezord
 usermod -a -G plugdev trezord
-touch /var/log/trezord.log
-chown trezord:trezord /var/log/trezord.log
-chmod 660 /var/log/trezord.log
