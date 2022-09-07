24d81500a2a13db471ed6479fcd909f938da4153
Igor Hnizdo <igor.hnizdo.345@proton.me>
2022-09-07 16:50:13 +0200

    Build Linux binaries with musl
    When I tried to build with "normal" glibc, I got weird errors on old/new glibc.
    
    Instead of building with glibc, which limits us with compatibility and weird issues, let's build with
    musl, which will always produce 100% static library.
    
    (Musl is a libc alternative. The good thing is musl is statically linked
    and can cross-compile relatively easily.)
    
    The actual hard work is building all these musl cross-compilers;
    luckily, Rust people already did that for Rust :) we can steal that and reuse.
    
    Note that this all works on both AMD and ARM hosts, as both the musl cross-compilers images
    and the go images are published for both.

---
+++ release/linux/build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -ex
+PATH=$PATH:/usr/local/go/bin
+
+case $TREZORD_BUILD in
+  "go-arm64-musl")
+    CGO_ENABLED=1 CC="/usr/local/musl/bin/aarch64-unknown-linux-musl-gcc" GOARCH=arm64 \
+        go build --ldflags '-linkmode external -extldflags "-static" -extld /usr/local/musl/bin/aarch64-unknown-linux-musl-gcc' \
+        -o release/linux/build/trezord-linux-arm64
+    ;;
+
+  "go-386-musl")
+     CGO_ENABLED=1 CC="/usr/local/musl/bin/i686-unknown-linux-musl-gcc" GOARCH=386 \
+        go build --ldflags '-linkmode external -extldflags "-static" -extld /usr/local/musl/bin/i686-unknown-linux-musl-gcc' \
+        -o release/linux/build/trezord-linux-386
+    ;;
+
+  "go-amd64-musl")
+     CGO_ENABLED=1 CC="/usr/local/musl/bin/x86_64-unknown-linux-musl-gcc" GOARCH=amd64 \
+        go build --ldflags '-linkmode external -extldflags "-static" -extld /usr/local/musl/bin/x86_64-unknown-linux-musl-gcc' \
+        -o release/linux/build/trezord-linux-amd64
+    ;;
+
+  *)
+    echo -n "unknown build"
+    exit 1
+    ;;
+esac
